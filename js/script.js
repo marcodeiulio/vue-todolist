e4802ac25f2f35d7d17960b4622692356fad5782
marcodeiulio <mdeiulio@icloud.com>
2022-01-28 15:52:41 +0100

    - Added todo items

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@ Vue.config.devtools = true;
 const root = new Vue({
 	el: '#root',
 	data: {
-		myList: [
-
+		todos: [
+			{ text: 'Vue Link One', check: false },
+			{ text: 'Vue Link Two', check: false },
+			{ text: 'Vue Link Three', check: false },
 		],
 	},
 })
